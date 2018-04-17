49707ed5e71404c88b71c4d6a6cbc12f0667b11c
Andreas Gyr <andygyr@gmx.ch>
2018-04-17 11:23:57 +0200

    added basic swagger ui support

--- ShoppingListApi/Startup.cs
+++ ShoppingListApi/Startup.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -9,6 +10,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using NSwag.AspNetCore;
 using ShoppingListApi.Models;
 
 namespace ShoppingListApi
@@ -36,6 +38,8 @@ namespace ShoppingListApi
             {
                 app.UseDeveloperExceptionPage();
             }
+
+            app.UseSwaggerUi(typeof(Startup).GetTypeInfo().Assembly);
 
             app.UseMvc();
         }
